38cefcde7c394e940cffcddd6a080b27cd487d20
Je4n-A <jeanma6742@gmail.com>
2024-09-06 01:57:44 -0400

    "Add output functionality to display bill, tip , and total"

--- bill_splitter.js
+++ bill_splitter.js
@@ -3,6 +3,11 @@
 bill = 200
 let tip =  bill > 50;300 ? bill * 0.15: bill * 0.20;
 
+//Output Details//
+
+let total_amount = bill + tip; 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${total_amount}`) 
+
 
 
 
