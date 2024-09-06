4504601c3bb3bd66f52b129c355af61902053a78
Je4n-A <jeanma6742@gmail.com>
2024-09-06 12:32:34 -0400

    "Create and test calculateTip function"

--- bill_splitter.js
+++ bill_splitter.js
@@ -8,6 +8,32 @@ let tip =  bill > 50;300 ? bill * 0.15: bill * 0.20;
 let total_amount = bill + tip; 
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${total_amount}`) 
 
+//Create Function//
+
+function calculate_Tip(bill_amount){
+    let tippercentage;
+    
+    if(bill_amount < 50)
+        tippercentage = 0.2;
+    else if(bill_amount <= 300 && bill_amount >= 50) 
+        tippercentage = 0.15;
+    else 
+    tippercentage = 0.2;
+    let Tip = bill_amount * tippercentage;
+    return Tip;
+}
+    let bill_amount = 100;
+    let Tip = calculate_Tip(bill)
+    console.log(`Tip for ${bill_amount} bill is ${Tip}`)
+    
+
+    
+
+    
+
+
+    
+
 
 
 
